82370c6385e7475f7b7336b2b96bb368965f7253
Vlasdimir Samoylov <leammas@gmail.com>
2019-05-03 23:04:38 +0300

    w

--- modules/issue-tracker/src/test/scala/com/github/leammas/state/EventsourcedIssueSpec.scala
+++ modules/issue-tracker/src/test/scala/com/github/leammas/state/EventsourcedIssueSpec.scala
@@ -2,10 +2,16 @@ package com.github.leammas.state
 
 import java.util.UUID
 
+import cats.data.Chain
 import com.github.leammas.issue.issuetracker.IssueId
 import org.scalatest.{FlatSpec, Matchers}
 import wiring._
 import cats.syntax.either._
+import com.github.leammas.issue.issuetracker.IssueEvent.{
+  IssueCommentAdded,
+  IssueCreated,
+  IssueResolved
+}
 import com.github.leammas.testkit.syntax._
 
 class EventsourcedIssueSpec extends FlatSpec with Matchers {
@@ -22,7 +28,10 @@ class EventsourcedIssueSpec extends FlatSpec with Matchers {
 
     val s = ProcessState.init()
     result.run(s).unsafeRunSync() shouldEqual ().asRight
-    s.issues.store.get.unsafeRunSync()(issueId).length shouldEqual 3
+    s.issues.store.get.unsafeRunSync()(issueId) shouldEqual Chain(
+      IssueCreated(1, "foo"),
+      IssueCommentAdded("bar"),
+      IssueResolved)
     // check if events have been published
     s.issues.queue.dequeueCurrent.unsafeRunSync().length shouldEqual 3
   }
